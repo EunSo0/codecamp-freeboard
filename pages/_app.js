f07e60dd40f5122481b9ef2d8687b15337590977
EunSo0 <dmstn423@naver.com>
2023-09-14 15:02:48 +0900

    컴포넌트 분리

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const client = new ApolloClient({
-    uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
   });
 
   return (
